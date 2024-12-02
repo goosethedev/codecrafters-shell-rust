6aeccf51840e5d1aba66705d4b5e61f9a4a570d6
Gus Sánchez <goosethedev@proton.me>
2024-12-02 15:04:43 -0400

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@
 use std::io::{self, Write};
 
 fn main() {
-    // Uncomment this block to pass the first stage
     print!("$ ");
     io::stdout().flush().unwrap();
 
@@ -10,4 +9,7 @@ fn main() {
     let stdin = io::stdin();
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
+
+    // Hardcoded for now
+    println!("{}: command not found", input.trim());
 }
